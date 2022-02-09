443f52189588155a5a9c25252dd1777491b1feb6
Michael Moll <michael_moll@outlook.com>
2022-02-09 06:25:21 -0600

    Initial Commit

---
+++ Lesson-2-Gitub-And-XCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson-2-Gitub-And-XCode
+//
+//  Created by Michael Moll on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson-2-Gitub-And-XCode/Lesson_2_Gitub_And_XCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_2_Gitub_And_XCodeApp.swift
+//  Lesson-2-Gitub-And-XCode
+//
+//  Created by Michael Moll on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_2_Gitub_And_XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
